03409afb12eca1ae709e162f4fcf3f633b00ac45
Gustavo Ariel Coronel <ariel256@gmail.com>
2025-06-10 15:54:25 -0300

    Testing the consola H2

--- src/main/java/com/keko/peliculas/controllers/GeneroController.java
+++ src/main/java/com/keko/peliculas/controllers/GeneroController.java
@@ -2,15 +2,16 @@ package com.keko.peliculas.controllers;
 
 import com.keko.peliculas.dao.IGeneroRepository;
 import com.keko.peliculas.entities.Genero;
+import com.keko.peliculas.services.IGeneroService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class GeneroController {
 
-    private IGeneroRepository generoRepository;
+    private IGeneroService service;
 
-    public GeneroController(IGeneroRepository generoRepository) {
-        this.generoRepository = generoRepository;
+    public GeneroController(IGeneroService service) {
+        this.service = service;
     }
 
     @PostMapping("genero")
@@ -18,14 +19,13 @@ public class GeneroController {
         Genero genero = new Genero();
         genero.setNombre(nombre);
 
-        generoRepository.save(genero);
+        service.save(genero);
 
         return genero.getId();
     }
 
     @GetMapping("genero/{id}")
     public String buscarPorId(@PathVariable(name = "id") Long id) {
-        // return generoRepository.findById(id).getNombre();
-        return null;
+        return service.findById(id).getNombre();
     }
 }
